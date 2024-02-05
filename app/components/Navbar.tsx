8b98fde04a49044b734883532b9e149e7ea06e47
Ruumaa <titankalbuesa01@gmail.com>
2024-02-06 00:28:08 +0700

    fix minor style

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,9 @@ const Navbar = ({ location }: Props) => {
       <nav className="shadow-lg sticky top-0 left-0 z-30 backdrop-filter backdrop-blur-lg bg-opacity-50 flex-grow flex">
         <div className="h-[80px] w-full flex items-center justify-center max-w-7xl px-3 mx-auto">
           <div className="flex items-center justify-center gap-2">
-            <h2 className="text-3xl font-bold font-sans">Weather Sphere</h2>
+            <h2 className="text-2xl sm:text-3xl font-bold font-sans whitespace-nowrap">
+              Weather Sphere
+            </h2>
             <CloudSunRain size={40} className="text-orange-600" />
           </div>
           <section className="ml-auto relative flex  items-center justify-end gap-4 mr-5 md:mr-0">
